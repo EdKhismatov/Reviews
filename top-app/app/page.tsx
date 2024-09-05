3f5723faed665b88c6de1dc4d73c168c3d2399bb
EdKhismatov <E.Khismatov@yandex.ru>
2024-09-05 19:37:08 +0200

    button

--- top-app/app/page.tsx
+++ top-app/app/page.tsx
@@ -4,6 +4,7 @@ import { Metadata } from 'next';
 import Htag from '@/components/Htag/Htag';
 import Button from '@/components/Button/Button';
 import P from '../components/P/P'
+import Tag from '@/components/Tag/Tag';
 
 export const metadata: Metadata = {
   title: 'Измененные данные',
@@ -19,6 +20,10 @@ export default function Home() {
       <P size={'s'}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim, tempora.</P>
       <P size={'m'}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim, tempora.</P>
       <P size={'l'}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim, tempora.</P>
+      <Tag size='s'>Gohst</Tag>
+      <Tag size='m' color='red'>Red</Tag>
+      <Tag size='s' color='green'>Green</Tag>
+      <Tag size='s' color='primary'>Primary</Tag>
     </div>
   );
 }
