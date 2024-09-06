7f8089eeaa40364304016531281aed2f84902652
EdKhismatov <E.Khismatov@yandex.ru>
2024-09-06 20:57:43 +0200

    grid

--- top-app/layout/Footer/Footer.tsx
+++ top-app/layout/Footer/Footer.tsx
@@ -10,6 +10,8 @@ export default function Footer({
   return (
     <footer {...props} className={cn(className, styles.footer)}>
       <div>OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены</div>
+      <a href='#' target='_blank'> Пользовательское соглашение</a>
+      <a href='#' target='_blank'> Политика конфиденциальности</a>
     </footer>
   );
 }
